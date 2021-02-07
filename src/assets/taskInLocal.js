1693d0c1edbe93c50fa98dafb5106a8e5b5daaf1
ricardo <ramn21@gmail.com>
2021-02-07 22:13:59 +0100

    create object task

---
+++ src/assets/taskInLocal.js
@@ -0,0 +1,14 @@
+import getLocalProject from './getLocal'
+
+const taskInLocal = (titleProject) => {
+  const result = getLocalProject('task');
+  const task = result.filter((item) => {
+    if (item.project === titleProject) {
+      return true;
+    }
+    return false;
+  });
+  return task;
+}
+
+export { taskInLocal as default }
