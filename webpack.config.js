a8aff6b783864d90275a41dc93fe085b2a716a5e
ricardo <ramn21@gmail.com>
2021-02-07 14:44:47 +0100

    setup webpack and initial files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtrarctPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html',
+    inject: 'body',
+  }),
+  new MiniCssExtrarctPlugin(),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     { loader: 'babel-loader' },
+      //   ],
+      // },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtrarctPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtrarctPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
