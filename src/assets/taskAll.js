42a220d2e4d6a54717504d20ac02b40b87acea97
ricardo <ramn21@gmail.com>
2021-02-08 02:48:15 +0100

    add display all task

---
+++ src/assets/taskAll.js
@@ -0,0 +1,42 @@
+
+
+const appending = (project, title, description, date, priority, done) => {
+  const container = document.querySelector('#cont');
+  const ulList = document.createElement('ul');
+  const liProjName = document.createElement('li');
+  const liProjTitle = document.createElement('li');
+  const liProjDesc = document.createElement('li');
+  const liProjDate = document.createElement('li');
+  const liProjDone = document.createElement('li');
+  const liProjprio = document.createElement('li');
+
+  ulList.id = 'allTodos';
+
+  liProjName.textContent = project;
+  liProjTitle.textContent = title;
+  liProjDesc.textContent = description;
+  liProjDate.textContent = date;
+  liProjDone.textContent = done;
+  liProjprio.textContent = priority
+  ulList.append(liProjName);
+  ulList.append(liProjTitle);
+  ulList.append(liProjDesc);
+  ulList.append(liProjDate);
+  ulList.append(liProjDone);
+  ulList.append(liProjprio);
+  container.append(ulList);
+};
+
+const displayAll = (alltasks) => {
+  alltasks.map((item) => {
+    appending(item.project, item.title, item.description, item.date, item.priority, item.done);
+  });
+};
+
+
+const taskAll = () => {
+  const result = JSON.parse(localStorage.getItem('task'));
+  displayAll(result);
+};
+
+export { taskAll as default };
