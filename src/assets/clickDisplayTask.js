6f100bb7fdf987446f52580986aa368535241226
ricardo <ramn21@gmail.com>
2021-02-08 02:47:46 +0100

    add event to display task

--- src/assets/clickDisplayTask.js
+++ src/assets/clickDisplayTask.js
@@ -2,28 +2,30 @@ import taskDisplayNoTask from './taskDisplayNoTask';
 import taskDisplayElements from './taskDisplayElements';
 import taskInLocal from './taskInLocal';
 import taskForm from  './taskForm';
+import taskAll from './taskAll'
 
 const clickDisplayTask = (element, sibling) => {
   element.addEventListener('click', (e) => {
     const task = taskInLocal(e.target.textContent);
-    console.log(task);
-    console.log(element)
-    console.log(e.target.dataset)
-    if (e.target.dataset.name && task.length === 0) {
+    if(e.target.dataset.name === 'All') {
+      sibling.textContent = "";
+      taskAll()
+      console.log('im all the shit')
+    }
+    if (e.target.dataset.name && task.length === 0 && e.target.dataset.name !== 'All') {
       console.log('task empty')
       sibling.textContent = "";
+      const form = taskForm(e.target.textContent)
+      sibling.append(form)
       taskDisplayNoTask(sibling, e.target.textContent);
     }
-    if(e.target.dataset.name && task.length !== 0 ){
+    if(e.target.dataset.name && task.length !== 0 && e.target.dataset.name !== 'All'){
       sibling.textContent = "";
-      console.log(task)
       const form = taskForm(e.target.textContent)
-      form.id = e.target.textContent
       sibling.append(form)
-      // task.map((item) => {
-      //   taskDisplayElements(sibling, item.title, item.description, item.date, item.priority, item.done);
-      // });
-      // taskDisplayNoTask(element, e.target.textContent);
+      task.map((item) => {
+        taskDisplayElements(sibling, item.title, item.description, item.date, item.priority, item.done);
+      });
     }
   });
 };
