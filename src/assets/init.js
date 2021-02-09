78ad7563b9e87170ca9e6a3a57441b664020f3d4
ricardo <ramn21@gmail.com>
2021-02-09 22:54:34 +0100

    add default task and projects

--- src/assets/init.js
+++ src/assets/init.js
@@ -1,16 +1,18 @@
 import formProject from './formNewProject';
 import project from './projectAll';
 import clickDisplayTask from './clickDisplayTask';
-import taskAll from './taskAll'
+import taskAll from './taskAll';
+import taskProjectDefault from './taskProjectDefault';
 
 const init = () => {
-  const containerProjects = document.querySelector('#my-projects')
-  const containerTasks = document.querySelector('#cont')
-  const addProjectBtn = document.querySelector('#addProj')
-  project()
-  taskAll()
-  formProject(addProjectBtn)
-  clickDisplayTask(containerProjects, containerTasks)
+  const containerProjects = document.querySelector('#my-projects');
+  const containerTasks = document.querySelector('#cont');
+  const addProjectBtn = document.querySelector('#addProj');
+  taskProjectDefault();
+  project();
+  taskAll();
+  formProject(addProjectBtn);
+  clickDisplayTask(containerProjects, containerTasks);
 }
 
 export {init as default}
