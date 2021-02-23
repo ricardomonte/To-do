9b9a6f20e0c7bafa02494b76efd5d25d1f613b53
ricardo <ramn21@gmail.com>
2021-02-23 16:47:46 +0100

    Add babel and jest

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,18 @@ module.exports = {
 
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: [
-      //     { loader: 'babel-loader' },
-      //   ],
-      // },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+        ],
+      },
+      {
+        resolve: {
+          extensions: ['*', '.js']
+        }
+      },
       {
         test: /\.html$/i,
         loader: 'html-loader',
