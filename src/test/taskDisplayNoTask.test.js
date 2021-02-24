e60921298c753e3d3a522bfae1a320b92c2930ac
ricardo <ramn21@gmail.com>
2021-02-24 18:58:27 +0100

    Add taskDisplayNoTask taskForm .test.js

---
+++ src/test/taskDisplayNoTask.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from '@jest/globals';
+import taskDisplayNoTask from '../assets/taskDisplayNoTask';
+
+document.body.innerHTML =
+  '<div id=cont></div>';
+
+const parentElement = document.querySelector('#cont')
+const value = 'dance';
+taskDisplayNoTask(parentElement, value)
+test('child element to be a div', () => {
+  const child = document.querySelector('#cont').firstElementChild.tagName;
+  expect(child).toBe('DIV')
+});
+
+test('element to have text "Your project dance dont have tasks"', () => {
+  const element = document.querySelector('#notask').textContent
+  expect(element).toBe('Your project dance dont have tasks')
+})
---
+++ src/test/taskForm.test.js
@@ -0,0 +1,12 @@
+import { expect, test } from '@jest/globals';
+import taskForm from '../assets/taskForm';
+
+test('returned element is a form', () => {
+  const form = taskForm('dance');
+  expect(form.tagName).toBe('FORM')
+});
+
+test('returned element is not a div', () => {
+  const form = taskForm('dance');
+  expect(form.tagName).not.toBe('DIV')
+});
