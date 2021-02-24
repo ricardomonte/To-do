1508525a2dd0cc591aa746cc79c252e22883ce1f
ricardo <ramn21@gmail.com>
2021-02-24 22:10:03 +0100

    Add taskInLocal taskIsIn taskOnly .test.js

---
+++ src/test/taskInLocal.test.js
@@ -0,0 +1,38 @@
+import { beforeEach, expect, jest, test } from '@jest/globals';
+import taskInLocal from '../assets/taskInLocal';
+import getLocalProject from '../assets/getLocal';
+
+jest.mock('../../src/assets/getLocal')
+
+const mock = {
+  project: 'dance',
+  title: 'cumbia',
+  description: 'practice cumbia',
+  date: '2021-02-14',
+  priority: 'Important',
+  done: false,
+};
+
+beforeEach(() => {
+  getLocalProject.mockClear()
+});
+
+test('returned element its an instance of an Array', () => {
+  getLocalProject.mockReturnValue([mock]);
+  const titleProject = 'dance';
+  const result = taskInLocal(titleProject)
+  expect(result).toBeInstanceOf(Array)
+});
+
+test('returned element is not an instance of String', () => {
+  getLocalProject.mockReturnValue([mock]);
+  const titleProject = 'dance';
+  const result = taskInLocal(titleProject)
+  expect(result).not.toBeInstanceOf(String)
+});
+
+test('getLocal is called', () => {
+  const titleProject = 'dance';
+  taskInLocal(titleProject)
+  expect(getLocalProject).toHaveBeenCalled()
+})
